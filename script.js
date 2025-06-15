9fa03ee569d4b97b9ff8611a907ae3bcd3544356
Kolade Faith <koladefaith1000@gmail.com>
2025-06-15 22:14:23 +0100

    Work done

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ class footballLineUp {
     constructor(named, position, number) {
         this.named = named
         this.position = position
-        this.number = number
     }
     add = () => {
         show.innerHTML += `
         <div class='player ${this.position}'>
      <div class="flex-container">
-    <p> ${this.position || "Position"}
+    <p class='position' style='color: black; font-size: 15px'> ${this.position || "Position"}
         <p/>
         <img
-           src="./images/person-2.png" style='width: 40px; border-radius: 20px'
+           src="./images/person-2.png" style='width: 35px; border-radius: 20px'
            alt="Player" />
           </div>
-        <p style='color: red'> ${this.named || "Name"}  <p/>
+        <p style='color: black; font-size: 15px'> ${this.named || "Name"}  <p/>
 </div>
 `
     }
 }
 const addPlayers = () => {
-    if (playerName.value === '' || jerseyNumber.value === '') {
-        alert('Player Name and Jersey Number must not be empty')
+    if (playerName.value === '') {
+        alert('Player Name must not be empty')
     }
     else {
-        let GK = new footballLineUp(playerName.value, position.value, jerseyNumber.value)
+        let GK = new footballLineUp(playerName.value, position.value)
         GK.add()
         playerName.value = ''
-        jerseyNumber.value = ''
     }
 
 }
